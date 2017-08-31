33ca488c7bb119990290e4ea0b04267e638b7823
IdahoEv <evan.dorn@lootcrate.com>
2017-08-31 10:24:28 -0700

    Games have a configurable number of random buttons

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
+import { randomNumberGenerator } from '../store/util';
+
+// initial data
+//   target
+//   x numbers for buttons
 class Game extends React.Component {
+  static propTypes = {
+    numberCount: PropTypes.number.isRequired,
+  };
+
+  constructor(props) {
+    super();
+    // this.target = 10 + Math.floor(40 * Math.random());
+    this.randomNumbers = Array.from({ length: props.numberCount }).map(() =>
+      randomNumberGenerator()
+    );
+  }
   render() {
     return (
-      <div>Game...</div>
+      <div>
+        <div id='target'>...</div>
+        <div id='random-numbers'>TESTING: {this.props.numberCount}
+         --
+          { this.randomNumbers }
+        </div>
+      </div>
     );
   }
 }
