0130c5db84bc53e62458f6509052a9298fa18627
IdahoEv <evan.dorn@lootcrate.com>
2017-08-31 09:51:00 -0700

    Starting scaffold for game

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,22 @@ const path = require('path');
 
 const config = {
   devtool: 'eval-source-map',
-  entry: './index.js',
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, ''),
+    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
   },
   module: {
     rules: [
       // { test: /\.txt$/, use: 'raw-loader' },
-      { test: /\.js$/, use: 'babel-loader' }
-    ]
+      { test: /\.js$/, use: 'babel-loader' },
+      { test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' }
+        ]
+      }]
   }
 };
 
